3cbbb7131a8da349d5ea6f10f492896e47b313e7
Chad Angelelli <chadangelelli@gmail.com>
2013-11-21 15:56:47 -0600

    adds interpreter stub & node.js call

--- bin/generate.bash
+++ bin/generate.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 jison lib/grammar.yy lib/lexer.l -o bin/manana_parser.js
-node bin/manana_parser.js examples/example1.manana
+cat bin/manana_parser.js lib/interpreter.js > bin/manana.js
+node bin/manana.js examples/example1.manana true
