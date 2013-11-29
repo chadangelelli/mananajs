87cba3984cc3374497574d083c7404a95d00f2f8
Chad Angelelli <chadangelelli@gmail.com>
2013-11-28 19:03:55 -0600

    cleans up IR

--- bin/generate.bash
+++ bin/generate.bash
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-jison lib/grammar.yy lib/lexer.l -o bin/manana_parser.js
+jison src/manana.yy src/manana.l -o bin/manana_parser.js
 node bin/manana_parser.js examples/example1.manana
-#cat bin/manana_parser.js lib/interpreter.js > bin/manana.js
-#node bin/manana.js examples/example1.manana
