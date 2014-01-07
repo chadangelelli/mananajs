1deadb65d28446c77b534f38f7d14db24b66d905
Chad Angelelli <chadangelelli@gmail.com>
2014-01-06 20:15:36 -0600

    syncing changes

--- _old/bin/generate.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-jison lib/grammar.yy lib/lexer.l -o bin/manana_parser.js
-node bin/manana_parser.js examples/example1.manana
-#cat bin/manana_parser.js lib/interpreter.js > bin/manana.js
-#node bin/manana.js examples/example1.manana
