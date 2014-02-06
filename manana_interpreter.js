278993974ca9184e7a6f0ecafd9e9a6e33fc9517
Chad Angelelli <chad@angelel.li>
2014-02-06 13:56:38 -0500

    adds interpreter stubs and test script

---
+++ manana_interpreter.js
@@ -0,0 +1,45 @@
+(function(exports) {
+ 
+  // _____________________________________________ Error-handling
+  function MananaError(message, loc) {
+    this.name = "MananaError";
+    this.message = message;
+    this.loc = loc;
+  } // end MananaError()
+
+  // _____________________________________________ Validation shorthand 
+  function isStr(v)  { return typeof v === "string"; }
+  function isInt(v)  { return typeof v === "number" && parseFloat(v) == parseInt(v, 10) && ! isNaN(v); }
+  function isArr(v)  { return Object.prototype.toString.call(v) === '[object Array]'; }
+  function isObj(v)  { return Object.prototype.toString.call(v) === '[object Object]'; }
+  function isNull(v) { return v === null; }
+
+  // _____________________________________________ Interpreter
+  function MananaInterpreter() {
+    console.log(exports);
+    this.code = '';
+    this.context = {};
+
+    this.evalForm = function(form, context) {
+    } // end MananaInterpreter.evalForm()
+
+    this.eval = function(code, context) {
+      var res;
+
+      this.code = code;
+      this.context = context || {};
+
+
+      return res;
+    } // end MananaInterpreter.eval()
+
+  } // end MananaInterpreter()
+
+  // _____________________________________________ Make available in both node.js & browser 
+  if (typeof window !== "undefined") {
+    window.MananaInterpreter = MananaInterpreter;
+  } else {
+    exports.MananaInterpreter = MananaInterpreter;
+  }
+
+})(typeof exports === "undefined" ? this['manana'] = {} : exports);
