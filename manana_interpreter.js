82b765b2cba813bb4eef003cae77ed164cd8b484
Chad Angelelli <chad@angelel.li>
2014-04-28 15:39:09 -0400

    formats If() code

--- manana_interpreter.js
+++ manana_interpreter.js
@@ -170,52 +170,35 @@
       v2 = self.evalForm(form.value_2, context);
 
       _cond_true = false;
-      if (cond === "true" && v1) {
-        _cond_true = true; 
-      } else if (cond === "false" && ! v1) {
-        _cond_true = true; 
-      } else if (cond === "==" && v1 == v2) {
-        _cond_true = true;
-      } else if (cond === "!=" && v1 != v2) {
-        _cond_true = true;
-      } else if (cond === ">=" && v1 >= v2) {
-        _cond_true = true;
-      } else if (cond === "<=" && v1 <= v2) {
-        _cond_true = true;
-      } else if (cond === "is") {
-        if (v2 === "Hash") {
-          _cond_true = isObj(v1);
-        } else if (v2 === "List") {
-          _cond_true = isArr(v1);
-        } else if (v2 === "String") {
-          _cond_true = isStr(v1);
-        } else if (v2 === "Number") {
-          _cond_true = isNum(v1);
-        } else if (v2 === "Integer") {
-          _cond_true = isInt(v1);
-        }
+      if      (cond === "true"  && v1      ) _cond_true = true; 
+      else if (cond === "false" && ! v1    ) _cond_true = true; 
+      else if (cond === "=="    && v1 == v2) _cond_true = true;
+      else if (cond === "!="    && v1 != v2) _cond_true = true;
+      else if (cond === ">="    && v1 >= v2) _cond_true = true;
+      else if (cond === "<="    && v1 <= v2) _cond_true = true;
+
+      else if (cond === "is") {
+        if      (v2 === "Hash"   ) _cond_true = isObj(v1);
+        else if (v2 === "List"   ) _cond_true = isArr(v1);
+        else if (v2 === "String" ) _cond_true = isStr(v1);
+        else if (v2 === "Number" ) _cond_true = isNum(v1);
+        else if (v2 === "Integer") _cond_true = isInt(v1);
+        
       } else if (cond === "is not" && ! is(v1, v2)) {
-        if (v2 === "Hash") {
-          _cond_true = isObj(v1);
-        } else if (v2 === "List") {
-          _cond_true = ! isArr(v1);
-        } else if (v2 === "String") {
-          _cond_true = ! isStr(v1);
-        } else if (v2 === "Number") {
-          _cond_true = ! isNum(v1);
-        } else if (v2 === "Integer") {
-          _cond_true = ! isInt(v1);
-        }
+        if      (v2 === "Hash"   ) _cond_true = ! isObj(v1);
+        else if (v2 === "List"   ) _cond_true = ! isArr(v1);
+        else if (v2 === "String" ) _cond_true = ! isStr(v1);
+        else if (v2 === "Number" ) _cond_true = ! isNum(v1);
+        else if (v2 === "Integer") _cond_true = ! isInt(v1);
+        
       } else if (cond === "in") {
-        if (isArr(v2) && v2.indexOf(v1) > -1) {
-          _cond_true = true;
-        } else if (isObj(v2) && v1 in v2) {
-          _cond_true = true;
-        }
+        if      (isArr(v2) && v2.indexOf(v1) > -1) _cond_true = true;
+        else if (isObj(v2) && v1 in v2           ) _cond_true = true;
       }
 
       console.log("\n\n\n\n" + _cond_true + "\n\n\n\n");
       if (_cond_true) {
+        console.log(JSON.stringify(form.body));
         res = self.evalForm(form.body, context);
       } else if ( ! isNull(form.else_body)) {
         res = self.evalForm(form.else_body, context);
