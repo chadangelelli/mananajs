0085e7c95c79f3acf457ba9e8995b619a138ba99
Chad Angelelli <chad@angelel.li>
2014-07-25 12:16:46 -0500

    adds  to Path()

--- manana_interpreter.js
+++ manana_interpreter.js
@@ -385,6 +385,9 @@
         if (target == "$manana") {
           node = self;
 
+        } else if (target == "$window") {
+          node = window;
+
         } else if (self.isNamespace(node)) {
           if ( ! isNull(node.data) && ! is(node.data[target], "undefined")) {
             node = node.data[target];
