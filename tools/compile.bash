1ee890e53a3bc97783a790477a90765fc2c4efad
Chad Angelelli <chad@angelel.li>
2014-11-09 12:20:55 -0500

    changes compiler, starting IDE code

--- tools/compile.bash
+++ tools/compile.bash
@@ -4,8 +4,6 @@
 # https://developers.google.com/closure/compiler/
 
 pwd
-cat lib/manana_parser.js lib/manana_interpreter.js > bin/manana_src.js
+cat lib/manana_parser.js lib/manana_interpreter.js > bin/manana.uncompiled.js
 
-java -jar tools/closure-compiler.jar --js bin/manana_src.js --js_output_file bin/manana.js
-
-rm bin/manana_src.js
+java -jar tools/closure-compiler.jar --js bin/manana.uncompiled.js --js_output_file bin/manana.js
