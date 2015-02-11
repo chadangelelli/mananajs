baea98dd611bc96fb986d0a4afc17db3dc9b69d4
Chad Angelelli <chad@angelel.li>
2015-02-11 08:29:47 -0500

    fixes scoping/path issues

--- tools/test.js
+++ tools/test.js
@@ -10,7 +10,10 @@ basedir = basedir.join('/');
 var view = basedir + '/' + args[0];
 var context = require(basedir + '/' + args[1]);
 
+/*
 var Manana = require(basedir + '/bin/manana').Manana;
+*/
+var Manana = require(basedir + '/lib/manana_interpreter').Manana;
 var manana = new Manana(basedir);
 
 var res = manana.render(view, context); 
